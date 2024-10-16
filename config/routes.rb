a5002d061243ec23d7efc628bf1eb98af4a55083
stskri <177446189+stskri@users.noreply.github.com>
2024-10-16 12:11:27 +0000

    Add root_path + a

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,20 @@
 Rails.application.routes.draw do
   get 'homes/top'
   get 'homes/about'
+  
+  root :to =>"homes#top"
+  get "home/about"=>"homes#about"
+  
 # 顧客用
 # URL /customers/sign_in ...
-devise_for :customers,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
 
 # 管理者用
 # URL /admin/sign_in ...
-devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
 end
