7751f88153e716aff11539e0f9194ae070c3f1bd
stskri <177446189+stskri@users.noreply.github.com>
2024-10-16 09:16:45 +0000

    Add model to OrderDetail

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2024_10_16_071108) do
+ActiveRecord::Schema.define(version: 2024_10_16_090705) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -41,8 +40,6 @@ ActiveRecord::Schema.define(version: 2024_10_16_071108) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-ActiveRecord::Schema.define(version: 2024_10_16_065101) do
-
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -67,19 +64,16 @@ ActiveRecord::Schema.define(version: 2024_10_16_065101) do
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
+  create_table "order_details", force: :cascade do |t|
+    t.integer "item_id", null: false
+    t.integer "order_id", null: false
+    t.integer "price", null: false
+    t.integer "amount", null: false
+    t.integer "making_status", default: 0, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-
- end
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
 end
