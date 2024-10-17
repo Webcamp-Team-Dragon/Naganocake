4266c8f08c7bf8383cf0a611add71d36cf572cc2
tatu0321 <177841065+tatu0321@users.noreply.github.com>
2024-10-17 05:17:42 +0000

    test

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,15 +1,9 @@
 class Public::HomesController < ApplicationController
-<<<<<<< HEAD
-  def top
-  end
-
-=======
   # GET	/
   def top
   end
 
 # GET	/about
->>>>>>> 732e50abb4e6ae5a1aa47f2a3dafd98f9f6a2210
   def about
   end
 end
