fc2100f924a61c8317e2760a5e4e11c9bc8e30d2
tatu0321 <177841065+tatu0321@users.noreply.github.com>
2024-10-17 04:54:19 +0000

    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,16 @@
 Rails.application.routes.draw do
-  get 'homes/top'
-  get 'homes/about'
-  
-  root :to =>"homes#top"
-  get "home/about"=>"homes#about"
-  
-# 顧客用
-# URL /customers/sign_in ...
-  devise_for :customers,skip: [:passwords], controllers: {
+  root :to =>"publock/homes#top"
+
+  # 顧客用
+  # URL /customers/sign_in ...
+  devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
 
-# 管理者用
-# URL /admin/sign_in ...
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
 end
