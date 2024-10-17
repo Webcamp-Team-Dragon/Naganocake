72c0716d03b2d0dd8cb0556dbbea48765f9fe0c8
EC2 Default User <ec2-user@ip-172-31-7-137.ap-northeast-1.compute.internal>
2024-10-17 04:38:25 +0000

    controller追加

---
+++ app/controllers/admin/customers_controller.rb
@@ -0,0 +1,17 @@
+class Admin::CustomersController < ApplicationController
+# GET	/admin/customers
+  def index
+  end
+
+# GET	/admin/customers/:id
+  def show
+  end
+
+# GET	/admin/customers/:id/edit
+  def edit
+  end
+
+# PATCH	/admin/customers/:id
+  def update
+  end
+end
---
+++ app/controllers/admin/genres_controller.rb
@@ -0,0 +1,17 @@
+class Admin::GenresController < ApplicationController
+  # GET	/admin/genres
+  def index
+  end
+
+# POST	/admin/genres
+  def create
+  end
+
+# GET	/admin/genres/:id/edit
+  def edit
+  end
+
+# PATCH	/admin/genres/:id
+  def update
+  end
+end
