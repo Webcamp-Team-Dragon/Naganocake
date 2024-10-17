8c0db7efc78b44eb3c83bf3a4276a47f3d0d421e
EC2 Default User <ec2-user@ip-172-31-7-137.ap-northeast-1.compute.internal>
2024-10-17 09:06:16 +0000

    [Add]item, genre seed

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,11 @@
 class Public::ItemsController < ApplicationController
+  # GET	/items
   def index
+    @items = Item.all
   end
 
+# GET	/items/:id
   def show
+    @item = Item.find(params[:id])
   end
 end
