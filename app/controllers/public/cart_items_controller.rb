7335712b507050e40bc76f251486d077467e2032
EC2 Default User <ec2-user@ip-172-31-7-137.ap-northeast-1.compute.internal>
2024-10-18 07:02:32 +0000

    cart_in

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -17,10 +17,12 @@ class Public::CartItemsController < ApplicationController
 
 # POST	/cart_items
   def create
-    @cart_item = CartItem.new(item_id: params[:item_id], quantity: params[:quantity])
+    @cart_item = CartItem.new(items_id: params[:item_id], amount: params[:quantity])
     if @cart_item.save
       redirect_to public_cart_items_path, notice: '商品がカートに追加されました'
     else
-      redirect_to item_path(params[:item_id]), alert: 'カートに追加できませんでした'
+      redirect_to public_item_path(params[:item_id]), alert: '商品をカートに追加できませんでした'
     end
   end
+
+end
