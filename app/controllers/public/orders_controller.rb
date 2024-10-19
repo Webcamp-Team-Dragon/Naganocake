27ca88c123b925937d80f344714cc50da864942b
stskri <177446189+stskri@users.noreply.github.com>
2024-10-19 04:04:25 +0000

    20241019_13:04

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -2,6 +2,7 @@ class Public::OrdersController < ApplicationController
   def new
     @order = Order.new
     @address = current_customer.address
+    @addresses = Address.all
   end
 
   def confirm
@@ -11,6 +12,16 @@ class Public::OrdersController < ApplicationController
   end
 
   def create
+    @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    @address.save
+    if @order.save
+      redirect_to public_orders_confirm
+    else
+      redirect_to request.referer
+    end
   end
 
   def index
@@ -18,4 +29,14 @@ class Public::OrdersController < ApplicationController
 
   def show
   end
+
+  private
+  def order_params
+    params.require(:order).permit(:name, :address, :postal_code, :payment_method, :total_payment, :shipping_cost, :status)
+  end
+
+  def address_params
+    params.require(:address).permit(:postal_code, :address, :name)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
 
-  scope module: :public do
-    resources :orders
-    get 'orders/confirm'
-    get 'orders/thanks'
-  end
-
   namespace :admin do
     get 'order_details/update'
     get 'orders/show'
@@ -26,10 +20,10 @@ Rails.application.routes.draw do
     get 'items/show'
     get 'items/edit'
     get 'items/update'
-    
+
      get '/' => 'homes#top'
      root :to =>"admin/homes#top"
-     
+
   end
 
   namespace :public do
@@ -43,15 +37,15 @@ Rails.application.routes.draw do
     resources :addresses, only: [:new, :index, :edit, :create, :update, :destroy]
   end
 
-<<<<<<< HEAD
   namespace :public do
     # get 'orders/new'
-    # get 'orders/confirm'
-    # get 'orders/thanks'
+    get 'orders/confirm'
+    get 'orders/thanks'
     # get 'orders/create'
     # get 'orders/index'
     # get 'orders/show'
-    # resources :orders
+    resources :orders
+
   end
   namespace :public do
     get 'cart_items/index'
@@ -59,19 +53,6 @@ Rails.application.routes.draw do
     get 'cart_items/destroy'
     get 'cart_items/destroy_all'
     get 'cart_items/create'
-=======
-    get 'addresses/index'
-    get 'addresses/edit'
-    get 'addresses/create'
-    get 'addresses/update'
-    get 'addresses/destroy'
-    get 'orders/new'
-    get 'orders/confirm'
-    get 'orders/thanks'
-    get 'orders/create'
-    get 'orders/index'
-    get 'orders/show'
->>>>>>> develop
     get 'customers/show'
     get 'customers/edit'
     get 'customers/update'
@@ -90,14 +71,14 @@ Rails.application.routes.draw do
 
 # 顧客用
 # URL /customers/sign_in ...
-  devise_for :customers,skip: [:passwords], controllers: {
-    registrations: "public/registrations",
-    sessions: 'public/sessions'
-  }
+devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
 
-  # 管理者用
-  # URL /admin/sign_in ...
-  devise_for :admin, skip: [:registrations, :passwords], controllers: {
-    sessions: "admin/sessions"
-  }
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
 end
