c6f16c9869c7ad5149eff16ea5e456b206913267
EC2 Default User <ec2-user@ip-172-31-7-137.ap-northeast-1.compute.internal>
2024-10-19 05:22:39 +0000

    cart_itemテーブルの修正

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,5 @@
 class Public::CartItemsController < ApplicationController
+
   # GET	/cart_items
   def index
   end
@@ -18,16 +19,22 @@ class Public::CartItemsController < ApplicationController
 # POST	/cart_items
   def create
     if customer_signed_in?
-      @cart_item = CartItem.new(items_id: params[:item_id], amount: params[:quantity])
-      if @cart_item.save
-        redirect_to public_cart_items_path, notice: '商品がカートに追加されました'
+      quantity = params[:quantity].to_i  # フォームから送信されたquantityは文字列として受け取られます。to_iを使って整数に変換します。
+      logger.debug "Quantity: #{quantity}, Item ID: #{params[:item_id]}"
+      if quantity > 0  # フォームで選択された数量が0より大きいかをチェックしています。
+        @cart_item = CartItem.new(item_id: params[:item_id], amount: quantity, customer_id: current_customer.id)
+        if @cart_item.save
+          redirect_to public_cart_items_path, notice: '商品がカートに追加されました'
+        else
+          # logger.debug @cart_item.errors.full_messages  エラーメッセージをログに出力
+          redirect_to public_item_path(params[:item_id]), alert: '商品をカートに追加できませんでした'
+        end
       else
-        redirect_to public_item_path(params[:item_id]), alert: '商品をカートに追加できませんでした'
+        redirect_to public_item_path(params[:item_id]), alert: '個数を選択してください'
       end
     else
       flash[:alert] = "ログインしてください。"
       redirect_to public_item_path(params[:item_id])
     end
   end
-
 end
