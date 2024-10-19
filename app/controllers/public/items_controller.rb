7c09fcf82c88360f32b6b186e2127fad195a6492
EC2 Default User <ec2-user@ip-172-31-7-137.ap-northeast-1.compute.internal>
2024-10-19 06:59:57 +0000

    cart内一覧ページ#

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,6 +1,8 @@
 class Public::ItemsController < ApplicationController
   # GET	/items
   def index
+    # @items = Item.all
+    @items = Item.page(params[:page]).per(8) # 1ページあたり5件
     @item = Item.page(params[:page])
     @total_items = Item.count
     @genres = Genre.all
