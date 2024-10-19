8d1c4dcd9cf0d0208159558cf33bcf87ca02cfb1
stskri <177446189+stskri@users.noreply.github.com>
2024-10-19 14:32:30 +0000

    Add addreses/:id/edit + destroy

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -6,7 +6,7 @@ class Public::AddressesController < ApplicationController
   end
 
   def edit
-    @addresses = Address.all
+    @address = Address.find(params[:id])
   end
 
   def create
@@ -20,9 +20,18 @@ class Public::AddressesController < ApplicationController
   end
 
   def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      redirect_to addresses_path
+    else
+      render "edit"
+    end
   end
 
   def destroy
+    address = Address.find(params[:id])
+    address.destroy
+    redirect_to addresses_path
   end
 
 
