115ddcd6bc60862c08871df73268b5cedb409bb0
EC2 Default User <ec2-user@ip-172-31-7-137.ap-northeast-1.compute.internal>
2024-10-20 07:09:43 +0000

    public/orders/confirm.html.erb_table1

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,23 +6,37 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
-    @order = Order.new(order_params)
-    # ラジオボタンの選択に応じて住所情報を反映
-    if @order.address_type == "self"
+    @order = Order.new(order_params) # フォームから送信されたデータを使用して新しいOrderを作成
+
+    # 「ご自身の住所」を選択した場合
+    if params[:order][:address] == current_customer.address
       @order.name = current_customer.last_name + current_customer.first_name
       @order.address = current_customer.address
       @order.postal_code = current_customer.postal_code
-    elsif @order.address_type == "registered"
-      selected_address = Address.find(@order.registered_address)
+
+    # 「登録済住所から選択」を選択した場合
+    elsif params[:order][:address].to_i > 0
+      selected_address = Address.find(params[:order][:address])
       @order.name = selected_address.name
       @order.address = selected_address.address
       @order.postal_code = selected_address.postal_code
-    elsif @order.address_type == "new"
-      @order.name = @order.new_name
-      @order.address = @order.new_address
-      @order.postal_code = @order.new_postal_code
+
+    # 「新しいお届け先」を選択した場合
+    elsif params[:order][:address] == "new_address"
+      @order.name = params[:order][:new_name]
+      @order.address = params[:order][:new_address]
+      @order.postal_code = params[:order][:new_postal_code]
     end
+
+    # 支払方法の設定
+    @order.payment_method = params[:order][:pay_method]
+
+    # カートの合計金額と送料を設定（カートがある場合）
+    @cart_items = current_customer.cart_items
+    @order.shipping_cost ||= 0 # もし nil なら 0 を設定
+    @order.total_payment = @cart_items.sum(&:total_price) + @order.shipping_cost
   end
+
 
   def thanks
   end
